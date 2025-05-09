1f20988d70f0a7b123db4353a991f0e12a1d06a0
Oleh Tatarynov <exocriador@gmail.com>
2025-05-09 14:50:28 +0300

    added read+write utils, script for operate contacts

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,11 @@
-const generateContacts = async (number) => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+const generateContacts = async (number) => {
+    const contacts = await readContacts();
+    const newContacts = Array(number).fill(0).map(createFakeContact);
+    await writeContacts([...contacts, ...newContacts]);
+};
 
 generateContacts(5);
--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,3 +1,10 @@
-export const removeAllContacts = async () => {};
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
+export const removeAllContacts = async () => {
+    await readContacts();
+    await writeContacts([]);
+
+};
 
 removeAllContacts();
